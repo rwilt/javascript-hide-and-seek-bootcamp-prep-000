5f891f08cae492b81ce22094234a5b41139161d8
rosie w <rosie.wilt@gmail.com>
2018-02-26 02:39:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,19 @@ return document.querySelector(selector)
 function nestedTarget() {
   return document.querySelector(`#nested .target`)
 
+}
 
+// <ul class="ranked-list">
+//   <li>1</li>
+//   <li>2</li>
+// </ul>
+//
+// <ul class="ranked-list">
+//   <li>12</li>
+//   <li>11</li>
+//   <li>10</li>
+// </ul>
+
+function increaseRankBy(n) {
+  
 }
