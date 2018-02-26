3729e8c9442c48b479f84245d791b5e582b3f615
rosie w <rosie.wilt@gmail.com>
2018-02-26 02:01:10 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function getFirstSelector(selector)  {
+
+console.log(document.querySelector(id))
+
+}
+
+function nestedTarget() {
+  var d = nested.target
+console.log (document.getElementById(d))
+
+}
